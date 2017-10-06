4d3e711007d020fc8e385a69cda32b8903875507
Jan Stanley Go <stango97@gmail.com>
2017-10-06 11:18:06 -0400

    Delete PratisMain.php

--- PratisMain.php
+++
@@ -1,6 +0,0 @@
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('w92EXt5ExG8ag/GKqIUhVt5qy+O63q2guL++8W2d2ry9nWKABy0YVAg0A3GU2ctfC3j3WHZljkoAGqH4YHoXWBSVA09YRnCtSgIA+A3SHjIBWYvb8dWqHXm+RBICuToSF+A87TtUM26QZJcieMwcXwdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'd0c393557c41e139b951fb4a74ae9d63']);
-
-
-$response = $bot->replyText('Hello Prati', 'hello!');
-
